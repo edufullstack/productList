5f000c79502f78ad458eee2f736cbd4c5f723274
jose <joseerahe@gmail.com>
2023-03-25 09:37:06 -0700

    'connecting database 1/2'

--- back/index.js
+++ back/index.js
@@ -1,8 +1,10 @@
 const server = require('./src/app.js')
 const { database } = require('./src/db')
+require('dotenv').config()
+const { PORT } = process.env
 
 database.sync({ force: false }).then(() =>
-  server.listen(3001, () => {
-    console.log('Server on port 3001')
+  server.listen(PORT, () => {
+    console.log('Server on port ', PORT)
   })
 )
--- back/src/db.js
+++ back/src/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const producto = require('./modelos/productos')
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env
 
 const database = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false,
   }
